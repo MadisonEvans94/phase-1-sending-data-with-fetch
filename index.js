c0045cbc89a8a7607858e12bf204d7d756adcbb9
Madison Evans <m.corbinevans@gmail.com>
2023-01-17 07:47:46 -0700

    test passing

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+document.addEventListener("DOMContentLoaded", submitData);
+
+function submitData(name = "Steve", email = "steve@steve.com") {
+	const url = "http://localhost:3000/users";
+	const userData = {
+		name: name,
+		email: email,
+	};
+	const configurationObject = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(userData),
+	};
+	return fetch(url, configurationObject)
+		.then((res) => res.json())
+		.then((resID) => {
+			document.body.innerHTML = resID.id;
+		})
+		.catch((error) => {
+			document.body.append(error.message);
+		});
+}
